4d1d877d0df4804d408c688c8f72a944be731f35
Rahad2u <94119962+Rahad2u@users.noreply.github.com>
2022-12-29 18:26:19 +0600

    add comment delete api

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 const jwt = require('jsonwebtoken');
 const { query } = require('express');
 const port = process.env.PORT || 5000;
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 
 // midle ware 
@@ -56,19 +56,26 @@ app.post('/comments', async(req, res)=>{
     }
 })
 // comment get api
-app.get('/comments', async(req, res)=>{
+app.get('/comments', async(req, res)=>{ 
     const query = {};
     const sortingComment =  postcomment.find(query).sort({
         commentTime: -1
     });
-    const result = await sortingComment.toArray();
+    const result = await sortingComment.toArray(); 
+    res.send(result); 
+})
+//  comments delete api
+app.delete('/comments/:id', async(req, res)=>{
+    const id = req.params.id;
+    // console.log(id);
+    const query = {_id: ObjectId(id)};
+    const result = await postcomment.deleteOne(query);
     res.send(result);
 })
-
 app.get('/', (req, res)=>{
     res.send('server is running');
 })
 
 app.listen(port, ()=>{
     console.log(`server running ${port} port`)
-})
+}) 
